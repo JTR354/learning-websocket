5ea8ebbb16e772c1cb36ceb94f61abca93ac7e7c
jtr354 <jtr354@foxmail.com>
2022-10-03 16:33:02 +0800

    feat: 2-2 chat's name,num,logout tips

--- src/2/server/index.js
+++ src/2/server/index.js
@@ -3,12 +3,30 @@ const WebSocket = require("ws");
 const port = 3000;
 const wss = new WebSocket.Server({ port });
 
+let num = 0;
 wss.on("connection", (ws) => {
   ws.on("message", (msg) => {
     console.log("this is from client" + msg);
+    const { event, message, ...others } = JSON.parse(msg);
+    if (event === "enter") {
+      ws.name = message;
+      num++;
+    }
     wss.clients.forEach((client) => {
-      if (ws !== client && client.readyState === WebSocket.OPEN) {
-        client.send(msg.toString());
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(
+          JSON.stringify({ ...others, message, event, num, name: ws.name })
+        );
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    if (!ws.name) return;
+    num--;
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ event: "logout", num, name: ws.name }));
       }
     });
   });
