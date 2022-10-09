c66ad663ecd5f25828f31e585617ffd8f9846e74
jtr354 <jtr354@foxmail.com>
2022-10-09 17:38:35 +0800

    chore: commitlint 300 max length

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [2, "always", ["feat", "fix", "revert", "chore"]],
-    "subject-max-length": [1, "always", 30],
+    "subject-max-length": [1, "always", 300],
   },
 };
